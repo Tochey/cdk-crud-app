6c282542e68f3c59a56351d0d0a721ef2a59dbfc
DON <don@DON-4.local>
2022-09-12 03:28:18 -0400

    create portion complete

--- lib/crud-cdk-stack.ts
+++ lib/crud-cdk-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 export class CrudCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const employeeTable = new dynamodb.Table(this, "employeeRecords", {
+      partitionKey : {
+        name : 'id', 
+        type : dynamodb.AttributeType.STRING
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
+    
+    const createEmployee = new lambda.Function(this, "createEmployee", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler : 'createEmployee.handler',
+      environment : {
+        EMPLOYEE_TABLE_NAME : employeeTable.tableName
+      }
+    })
+    
+    const api = new apigw.LambdaRestApi(this, "lambdaApi", {
+      handler : createEmployee,
+      proxy: true
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CrudCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    employeeTable.grantWriteData(createEmployee)
   }
 }
